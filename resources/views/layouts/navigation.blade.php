2338391728295a221a129713bec0163fe01ab6d3
Alex Garrett-Smith <alex@codecourse.com>
2024-09-10 10:05:23 +0100

    Episode 05

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('plans')" :active="request()->routeIs('plans')">
+                        {{ __('Plans') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PlanController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+
+Route::get('/plans', [PlanController::class, 'index'])
+    ->name('plans');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
