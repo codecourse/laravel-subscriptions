5a56383894ac04326603b9189c9e19b7b3d8b203
Alex Garrett-Smith <alex@codecourse.com>
2024-09-10 09:54:19 +0100

    Episode 04

---
+++ config/subscriptions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'plans' => [
+        'monthly' => [
+            'name' => 'Monthly',
+            'price_id' => 'price_1PxPmtJ8zTPUENgLecfpBCmB'
+        ],
+        'yearly' => [
+            'name' => 'Yearly',
+            'price_id' => 'price_1PxPnOJ8zTPUENgLh6mt4tNg'
+        ],
+    ]
+];
